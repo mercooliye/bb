27b6f67ec864c784e22750c4a0dd5fe2e5fc8411
cool <mer>
2018-07-11 12:01:16 +0300

    Initial Commit

---
+++ beautybox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  beautybox
+//
+//  Created by Evgeniy Merkulov on 11.07.18.
+//  Copyright © 2018 Evgeniy Merkulov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ beautybox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  beautybox
+//
+//  Created by Evgeniy Merkulov on 11.07.18.
+//  Copyright © 2018 Evgeniy Merkulov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
